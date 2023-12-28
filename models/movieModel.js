1f467f625d3c9d0f1667a45fbf001940922d036f
eladjmc <eladjmc88@gmail.com>
2023-12-28 15:30:16 +0200

    try to fix cyclic file system

--- models/movieModel.js
+++ models/movieModel.js
@@ -5,9 +5,9 @@ const filePath = path.join(path.resolve(), 'data/movies.json');
 
 // Function to initialize the movies file if it doesn't exist
 const initializeMoviesFile = () => {
-  if (!fs.existsSync('./movies.json')) {
+  if (!fs.existsSync(filePath)) {
     // If the file doesn't exist, create it with an empty array or default content
-    fs.writeFileSync("./movies.json", JSON.stringify([]), 'utf8');
+    fs.writeFileSync(filePath, JSON.stringify([]), 'utf8');
     console.log("created file");
   }
 };
@@ -15,7 +15,7 @@ const initializeMoviesFile = () => {
 const readMoviesFromFile = () => {
   try {
     initializeMoviesFile(); // Ensure the file exists before reading
-    const fileData = fs.readFileSync("./movies.json", 'utf8');
+    const fileData = fs.readFileSync(filePath, 'utf8');
     return JSON.parse(fileData);
   } catch (error) {
     throw new Error('Error reading from file');
@@ -25,7 +25,7 @@ const readMoviesFromFile = () => {
 const writeMoviesToFile = (movies) => {
   try {
     initializeMoviesFile(); // Ensure the file exists before writing
-    fs.writeFileSync("./movies.json", JSON.stringify(movies), 'utf8');
+    fs.writeFileSync(filePath, JSON.stringify(movies), 'utf8');
   } catch (error) {
     throw new Error('Error writing to file');
   }
