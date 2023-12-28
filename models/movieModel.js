6b5e922b26b11bf8696a047287e178b3bf8c2146
eladjmc <eladjmc88@gmail.com>
2023-12-28 15:22:25 +0200

    try to fix cyclic file system

--- models/movieModel.js
+++ models/movieModel.js
@@ -5,36 +5,29 @@ const filePath = path.join(path.resolve(), 'data/movies.json');
 
 // Function to initialize the movies file if it doesn't exist
 const initializeMoviesFile = () => {
-  // Ensure the directory exists
-  const dirPath = path.dirname(filePath);
-  if (!fs.existsSync(dirPath)) {
-    fs.mkdirSync(dirPath, { recursive: true });
-  }
-
-  if (!fs.existsSync(filePath)) {
-    // If the file doesn't exist, create it with an empty array
-    fs.writeFileSync(filePath, JSON.stringify([], null, 2), 'utf8');
+  if (!fs.existsSync('./movies.json')) {
+    // If the file doesn't exist, create it with an empty array or default content
+    fs.writeFileSync("/movies.json", JSON.stringify([], null, 2), 'utf8');
+    console.log("created file");
   }
 };
 
 const readMoviesFromFile = () => {
   try {
     initializeMoviesFile(); // Ensure the file exists before reading
-    const fileData = fs.readFileSync(filePath, 'utf8');
+    const fileData = fs.readFileSync("./movies.json", 'utf8');
     return JSON.parse(fileData);
   } catch (error) {
-    console.error('Error reading from file:', error); // Detailed error logging
-    throw error;
+    throw new Error('Error reading from file');
   }
 };
 
 const writeMoviesToFile = (movies) => {
   try {
     initializeMoviesFile(); // Ensure the file exists before writing
-    fs.writeFileSync(filePath, JSON.stringify(movies, null, 2), 'utf8');
+    fs.writeFileSync("./movies.json", JSON.stringify(movies), 'utf8');
   } catch (error) {
-    console.error('Error writing to file:', error); // Detailed error logging
-    throw error;
+    throw new Error('Error writing to file');
   }
 };
 
