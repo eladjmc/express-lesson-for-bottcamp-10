285529a31c7d0238e5fc3f5e5070d0e7c22e5ba2
eladjmc <eladjmc88@gmail.com>
2023-12-28 14:57:08 +0200

    try to fix cyclic file system

--- models/movieModel.js
+++ models/movieModel.js
@@ -1,12 +1,12 @@
-import fs from 'fs';
+import s3fs from '@cyclic.sh/s3fs';
 import path from 'path';
 
-const filePath = path.join(path.resolve(), 'tmp/movies.json');
+const filePath = path.join(path.resolve(), 'data/movies.json');
 
 const readMoviesFromFile = () => {
   try {
     console.log(filePath);
-    const fileData = fs.readFileSync(filePath, 'utf8');
+    const fileData = s3fs.readFileSync(filePath, 'utf8');
     return JSON.parse(fileData);
   } catch (error) {
     throw new Error('Error reading from file');
@@ -15,7 +15,7 @@ const readMoviesFromFile = () => {
 
 const writeMoviesToFile = (movies) => {
   try {
-    fs.writeFileSync(filePath, JSON.stringify(movies), 'utf8');
+    s3fs.writeFileSync(filePath, JSON.stringify(movies), 'utf8');
   } catch (error) {
     throw new Error('Error writing to file');
   }
